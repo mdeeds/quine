fd99cbbc734ac07437decb9698477b595f18930b
mdeeds <mdeeds@gmail.com>
2025-07-17 20:27:13 -0700

    Factor out client.js

--- graph-test.js
+++ graph-test.js
@@ -1,161 +1,8 @@
 // @ts-check
 
-import { TextMatrix } from './text-matrix.js';
+import { GraphClient } from './worker/client.js';
 
 let uid = 1;
-
-/**
- * 
- * @param {Worker} graph 
- * @param {string} name 
- * @param {Object!} spec 
- */
-function createNode(graph, name, spec) {
-  graph.postMessage({
-    type: 'createNode',
-    payload: { name, spec }
-  });
-}
-
-/**
- * 
- * @param {Worker} graph 
- * @param {string} x 
- * @param {string} w 
- * @param {string} b 
- * @param {string} y 
- */
-function multiplyAdd(graph, x, w, b, y) {
-  graph.postMessage({
-    type: 'multiplyAdd',
-    payload: { x, w, b, y }
-  });
-}
-
-function loss(graph, { actual, expected }) {
-  graph.postMessage({
-    type: 'loss',
-    payload: { actual, expected }
-  });
-}
-
-async function getComponentsInBuildOrder(graph) {
-  const expectedRequestId = uid++;
-  return new Promise((resolve, reject) => {
-    const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
-      const { type, payload, requestId } = e.data;
-      // We only care about the response to our specific request.
-      // TODO: More robust is to pass an id into the message and wait for that id in the response.
-      if (type === 'getComponentsInBuildOrder' && requestId === expectedRequestId) {
-        resolve(payload.componentNames);
-      }
-    }
-    graph.addEventListener('message', messageHandler);
-    graph.postMessage({ type: 'getComponentsInBuildOrder', requestId: expectedRequestId });
-  });
-}
-
-/**
- * 
- * @param {Worker!} graph 
- * @param {string!} name 
- * @returns {Promise<{values: Float32Array!, gradients: Float32Array!}>}
- */
-async function getValues(graph, name) {
-  const expectedRequestId = uid++;
-  return new Promise((resolve, reject) => {
-    const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
-      const { type, payload, requestId } = e.data;
-      if (type === 'getValues' && requestId === expectedRequestId) {
-        resolve(payload);
-      }
-    }
-    graph.addEventListener('message', messageHandler);
-    graph.postMessage({ type: 'getValues', payload: { name }, requestId: expectedRequestId });
-  });
-}
-
-/**
- * 
- */
-async function getSpec(graph, name) {
-  const expectedRequestId = uid++;
-  return new Promise((resolve, reject) => {
-    const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
-      const { type, payload, requestId } = e.data;
-      if (type === 'getSpec' && requestId === expectedRequestId) {
-        resolve(payload.spec);
-      }
-    }
-    graph.addEventListener('message', messageHandler);
-    graph.postMessage({ type: 'getSpec', payload: { name }, requestId: expectedRequestId });
-  });
-}
-
-/**
- * 
- * @param {Worker} graph 
- * @returns {Promise<void>}
- */
-async function waitForReady(graph) {
-  // waitForReady does not post a message and needs no requestId.
-  return new Promise((resolve, reject) => {
-    const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
-      const { type } = e.data;
-      if (type === 'ready') {
-        console.log('Ready!');
-        resolve();
-      }
-    }
-    graph.addEventListener('message', messageHandler);
-  });
-}
-
-/**
- * 
- * @param {Worker} graph 
- * @returns {Promise<void>}
- */
-async function finish(graph) {
-  const expectedRequestId = uid++;
-  return new Promise((resolve, reject) => {
-    const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
-      const { type, payload, requestId } = e.data;
-      if (type === 'finish' && requestId === expectedRequestId) {
-        resolve();
-      }
-    }
-    graph.addEventListener('message', messageHandler);
-    graph.postMessage({ type: 'finish', requestId: expectedRequestId });
-  });
-}
-
-async function displayAllNodes(graph) {
-  const d = document.getElementById('output');
-  if (!d) {
-    throw new Error('Output div not found.');
-  }
-  const components = await getComponentsInBuildOrder(graph);
-  for (const component of components) {
-    if (component.type === 'node') {
-      await addNodeInfo(graph, component.name, d);
-    }
-  }
-}
-
-const textMatrixMap = new Map();
-async function addNodeInfo(graph, nodeName, container) {
-  let textMatrix = textMatrixMap.get(nodeName);
-  if (!textMatrix) {
-    const spec = await getSpec(graph, nodeName);
-    textMatrix = new TextMatrix(nodeName, spec);
-    textMatrixMap.set(nodeName, textMatrix);
-    container.appendChild(textMatrix.div);
-  }
-  const { values, gradients } = await getValues(graph, nodeName);
-  textMatrix.update(values, gradients);
-  return;
-}
 
 class GraphTest {
   constructor() {
@@ -164,10 +11,7 @@ class GraphTest {
 
   async init() {
     console.log('Initializing...');
-    const graph = new Worker('worker/script.js', { type: 'module' });
-    await waitForReady(graph);
-
-    console.log('Constructing graph...');
+    const graph = await GraphClient.create();
 
     // Construct a simple two-layer net.
 
@@ -179,22 +23,22 @@ class GraphTest {
     const hiddenSize = 3;
     const outputSize = 1;  // 1 value per output
 
-    createNode(graph, 'X', { height: batchSize, width: inputSize, nodeType: 'input' });
-    createNode(graph, 'W1', { height: inputSize, width: hiddenSize, nodeType: 'train' });
-    createNode(graph, 'B1', { height: 1, width: hiddenSize, nodeType: 'train' });
-    createNode(graph, 'Y1', { height: batchSize, width: hiddenSize, nodeType: 'intermediate' });
+    graph.createNode('X', { height: batchSize, width: inputSize, nodeType: 'input' });
+    graph.createNode('W1', { height: inputSize, width: hiddenSize, nodeType: 'train' });
+    graph.createNode('B1', { height: 1, width: hiddenSize, nodeType: 'train' });
+    graph.createNode('Y1', { height: batchSize, width: hiddenSize, nodeType: 'intermediate' });
 
-    multiplyAdd(graph, 'X', 'W1', 'B1', 'Y1');
+    graph.multiplyAdd({ x: 'X', w: 'W1', b: 'B1', y: 'Y1' });
 
-    createNode(graph, 'R1', { height: batchSize, width: hiddenSize, nodeType: 'intermediate' });
+    graph.createNode('R1', { height: batchSize, width: hiddenSize, nodeType: 'intermediate' });
     graph.postMessage({ type: 'relu', payload: { x: 'Y1', y: 'R1' } });
 
-    createNode(graph, 'W2', { height: hiddenSize, width: outputSize, nodeType: 'train' });
-    createNode(graph, 'B2', { height: outputSize, width: 1, nodeType: 'train' });
-    createNode(graph, 'Y2', { height: batchSize, width: outputSize, nodeType: 'intermediate' });
+    graph.createNode('W2', { height: hiddenSize, width: outputSize, nodeType: 'train' });
+    graph.createNode('B2', { height: outputSize, width: 1, nodeType: 'train' });
+    graph.createNode('Y2', { height: batchSize, width: outputSize, nodeType: 'intermediate' });
 
-    multiplyAdd(graph, 'Y1', 'W2', 'B2', 'Y2');
-    createNode(graph, 'Y', { height: batchSize, width: outputSize, nodeType: 'output' });
+    graph.multiplyAdd({ x: 'Y1', w: 'W2', b: 'B2', y: 'Y2' });
+    graph.createNode('Y', { height: batchSize, width: outputSize, nodeType: 'output' });
     graph.postMessage({ type: 'relu', payload: { x: 'Y2', y: 'Y' } });
 
     graph.postMessage({
@@ -207,75 +51,33 @@ class GraphTest {
       }
     });
 
-    createNode(graph, 'Expected', { height: batchSize, width: outputSize, nodeType: 'output' });
+    graph.createNode('Expected', { height: batchSize, width: outputSize, nodeType: 'output' });
     graph.postMessage({
       type: 'setValues', payload: { name: 'Expected', values: [0, 1, 1, 0] }
     });
 
+    graph.loss({ actual: 'Y', expected: 'Expected' });
 
-    loss(graph, { actual: 'Y', expected: 'Expected' });
-
-    this.graph = graph;
     {
       const b = document.createElement('button');
       b.innerText = 'Forward';
-      b.onclick = this.runForward.bind(this);
+      b.onclick = graph.runForward.bind(graph);
       document.body.appendChild(b);
     }
     {
       const b = document.createElement('button');
       b.innerText = 'Backward';
-      b.onclick = this.runBackward.bind(this);
+      b.onclick = graph.runBackward.bind(graph);
       document.body.appendChild(b);
     }
     {
       const b = document.createElement('button');
       b.innerText = 'run 10x';
-      b.onclick = this.runX.bind(this, 10);
+      b.onclick = graph.runX.bind(graph, 10);
       document.body.appendChild(b);
     }
-    this.run();
+    await graph.displayAllNodes();
   }
-
-  async run() {
-    await this.runForward();
-    await this.runBackward();
-  }
-
-  async runForward() {
-    const graph = this.graph;
-    if (!graph) {
-      throw new Error('Graph worker not initialized.');
-    }
-    graph.postMessage({ type: 'forward' });
-    finish(graph);
-
-    await displayAllNodes(graph);
-  }
-  async runBackward() {
-    const graph = this.graph;
-    if (!graph) {
-      throw new Error('Graph worker not initialized.');
-    }
-    graph.postMessage({ type: 'backwardAndAddGradient', payload: { learningRate: 0.05 } });
-    finish(graph);
-
-    await displayAllNodes(graph);
-  }
-
-  async runX(n) {
-    const graph = this.graph;
-    if (!graph) {
-      throw new Error('Graph worker not initialized.');
-    }
-    for (let i = 0; i < n; i++) {
-      graph.postMessage({ type: 'forward' });
-      graph.postMessage({ type: 'backwardAndAddGradient', payload: { learningRate: 0.05 } });
-    }
-    finish(graph);
-    await displayAllNodes(graph);
-  }
-
 }
 
 async function init() {
---
+++ worker/client.js
@@ -0,0 +1,182 @@
+
+// @ts-check
+
+import { TextMatrix } from '../text-matrix.js';
+/** @typedef {import('./api.js').MatrixSpec} MatrixSpec */
+
+export class GraphClient {
+  constructor(graphWorker) {
+    this.graph = graphWorker;
+    this.uid = 1;
+    this.textMatrixMap = new Map();
+  }
+
+  static async create() {
+    const graph = new Worker('worker/script.js', { type: 'module' });
+    const g = new GraphClient(graph);
+    await g.waitForReady();
+    return g;
+  }
+
+  postMessage(message) {
+    this.graph.postMessage(message);
+  }
+
+  /**
+   * 
+   * @param {string!} name 
+   * @param {MatrixSpec!} spec 
+   */
+  createNode(name, spec) {
+    this.graph.postMessage({
+      type: 'createNode',
+      payload: { name, spec }
+    });
+  }
+
+  multiplyAdd({ x, w, b, y }) {
+    this.graph.postMessage({
+      type: 'multiplyAdd',
+      payload: { x, w, b, y }
+    });
+  }
+
+  loss({ actual, expected }) {
+    this.graph.postMessage({
+      type: 'loss',
+      payload: { actual, expected }
+    });
+  }
+
+  async getComponentsInBuildOrder() {
+    const expectedRequestId = this.uid++;
+    return new Promise((resolve, reject) => {
+      const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
+        const { type, payload, requestId } = e.data;
+        if (type === 'getComponentsInBuildOrder' && requestId === expectedRequestId) {
+          resolve(payload.componentNames);
+        }
+      }
+      this.graph.addEventListener('message', messageHandler);
+      this.graph.postMessage({ type: 'getComponentsInBuildOrder', requestId: expectedRequestId });
+    });
+  }
+
+  async getValues(name) {
+    const expectedRequestId = this.uid++;
+    return new Promise((resolve, reject) => {
+      const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
+        const { type, payload, requestId } = e.data;
+        if (type === 'getValues' && requestId === expectedRequestId) {
+          resolve(payload);
+        }
+      }
+      this.graph.addEventListener('message', messageHandler);
+      this.graph.postMessage({ type: 'getValues', payload: { name }, requestId: expectedRequestId });
+    });
+  }
+
+  /**
+   * 
+   * @param {string!} name 
+   * @returns {Promise<MatrixSpec!>}
+   */
+  async getSpec(name) {
+    const expectedRequestId = this.uid++;
+    return new Promise((resolve, reject) => {
+      const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
+        const { type, payload, requestId } = e.data;
+        if (type === 'getSpec' && requestId === expectedRequestId) {
+          resolve(payload.spec);
+        }
+      }
+      this.graph.addEventListener('message', messageHandler);
+      this.graph.postMessage({ type: 'getSpec', payload: { name }, requestId: expectedRequestId });
+    });
+  }
+
+  /**
+   * 
+   * @returns {Promise<void>}
+   */
+  async waitForReady() {
+    return new Promise((resolve, reject) => {
+      const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
+        const { type } = e.data;
+        if (type === 'ready') {
+          console.log('Ready!');
+          resolve();
+        }
+      }
+      this.graph.addEventListener('message', messageHandler);
+    });
+  }
+
+  /**
+   * 
+   * @returns {Promise<void>}
+   */
+  async finish() {
+    const expectedRequestId = this.uid++;
+    return new Promise((resolve, reject) => {
+      const messageHandler = (/** @type {{ data: { type: string; payload: any; requestId: number}; }} */ e) => {
+        const { type, payload, requestId } = e.data;
+        if (type === 'finish' && requestId === expectedRequestId) {
+          resolve();
+        }
+      }
+      this.graph.addEventListener('message', messageHandler);
+      this.graph.postMessage({ type: 'finish', requestId: expectedRequestId });
+    });
+  }
+
+
+  async displayAllNodes() {
+    const d = document.getElementById('output');
+    if (!d) {
+      throw new Error('Output div not found.');
+    }
+    const components = await this.getComponentsInBuildOrder();
+    for (const component of components) {
+      if (component.type === 'node') {
+        await this.addNodeInfo(component.name, d);
+      }
+    }
+  }
+
+  async addNodeInfo(nodeName, container) {
+    let textMatrix = this.textMatrixMap.get(nodeName);
+    if (!textMatrix) {
+      const spec = await this.getSpec(nodeName);
+      textMatrix = new TextMatrix(nodeName, spec);
+      this.textMatrixMap.set(nodeName, textMatrix);
+      container.appendChild(textMatrix.div);
+    }
+    const { values, gradients } = await this.getValues(nodeName);
+    textMatrix.update(values, gradients);
+    return;
+  }
+
+  async runForward() {
+    this.graph.postMessage({ type: 'forward' });
+    this.finish();
+
+    await this.displayAllNodes();
+  }
+  async runBackward() {
+    this.graph.postMessage({ type: 'backwardAndAddGradient', payload: { learningRate: 0.05 } });
+    this.finish();
+
+    await this.displayAllNodes();
+  }
+
+  async runX(n) {
+    for (let i = 0; i < n; i++) {
+      this.graph.postMessage({ type: 'forward' });
+      this.graph.postMessage({ type: 'backwardAndAddGradient', payload: { learningRate: 0.05 } });
+    }
+    this.finish();
+    await this.displayAllNodes();
+  }
+
+}
